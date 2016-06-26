76223d9509175b74027a8a58b3cb7ec1e9421fcc
to-hutohu <tohu.soy@gmail.com>
2016-06-26 17:23:04 +0900

    画像の追加

---
+++ main.js
@@ -0,0 +1,67 @@
+window.addEventListener('load',init);
+
+var canvas;
+var ctx;
+var SCREEN_WIDTH = 800;
+var SCREEN_HEIGHT = 600;
+var Asset = {};
+Asset.image{};
+
+Assets.assets =[
+  { type: 'image', name: 'back', src:'assets/background.png'}
+  { type: 'image', name: 'box', src:'assets/mikan.png'}
+];
+
+Assets._loadImage = function(asset, onLoad){
+  var image = new Image();
+  image.src = asset.src;
+  image.onload = onLoad;
+  Asset.images[asset.name] = image;
+};
+
+Asset.loadAssets = function(onComplete){
+  var total = Assets.assets.length;
+  var loadCount = 0;
+
+  var onLoad = function(){
+    loadCount++:
+    if(loadCount >= total){
+      onComplete();
+    }
+  };
+
+  Asset.assets.forEach(function(asset){
+    switch(asset.type){
+      case 'image':
+        Asset._loadImage(asset, onLoad);
+        break;
+    }
+  });
+};
+
+//初期化
+
+function init(){
+  //ここに初期化処理を書く
+  canvas = document.getElementById('maincanvas');
+  canvas.width = SCREEN_WIDTH;
+  canvas.height = SCREEN_HEIGHT;
+  
+  ctx = canvas.getContext('2d');
+
+  Asset.loadAssets(function(){
+    requestAnimationFrame(update);
+  });
+}
+
+function update(){
+  requestAnimationFrame(update);
+  render();
+}
+
+function render(){
+  //全体をクリア
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.drawImage(Asset.images['back'],0,0);
+  ctx.drawImage(Asset.images['box'],0,0);
+}
